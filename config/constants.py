957fed646721eab5fc3f3dc6935db586b7d2d090
0xuser <0xuser@users.noreply.github.com>
2020-03-22 15:23:10 +0100

    edited search tags, schema dumps added

--- config/constants.py
+++ config/constants.py
@@ -3,13 +3,19 @@ DATABASE_CONFIG_YAML_PATH = "C:\\Users\\RTQ748\\Desktop\\elections-git\\config\\
 DATABASE_NAME = "election_db"
 #query : table name
 SEARCH_TAGS = {
-    "#trump" : "search_hashtag_trump",
-    "#putin": "search_hashtag_putin"
+    "#2020election" : "search_hashtag_2020election",
+    "#trump2020": "search_hashtag_trump2020",
+    "#donaldtrump": "search_hashtag_donaldtrump",
+    "#biden2020": "search_hashtag_biden2020",
+    "#biden": "search_hashtag_biden",
+    "#bernie2020": "search_hashtag_bernie2020",
+    "#berniesanders": "search_hashtag_berniesanders"
 }
 #username : user id
 TRACKING_USERS = {
     "realDonaldTrump": "25073877",
-    "JoeBiden": "939091"
+    "JoeBiden": "939091",
+    "berniesanders": "216776631"
 }
 
 SEARCH_TWEETS_FETCH_SIZE = 100
--- config/schema.sql
+++
@@ -1,1 +0,0 @@
--- TODO: paste schema sql
