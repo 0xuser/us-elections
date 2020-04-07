671ce65cdcb6604ac24143fcaf522670c3201455
Krystian <krystian.konecki@yahoo.com>
2020-04-07 13:24:45 +0200

    Updating overall page

--- my-app/src/pages/OverallPage.js
+++ my-app/src/pages/OverallPage.js
@@ -8,16 +8,113 @@ class OverallPage extends Component {
         super(props);
         this.state= {
                 from: "",
-                to: ""
+                to: "", 
+                trackedS: [],
+                data: []
         }
     }
 
-    updateData = (from,to) => {
-            this.setState({from: from.replace(/\//gi,"-"), to: to.replace(/\//gi,"-")})
+    componentDidMount(){
+        fetch(`${this.props.path}api/getTrackedSearches`)
+        .then(resp => resp.json())
+        .then(data => {
+            const tags = [];
+            data.forEach(single => {
+                tags.push(single.search)
+            })
+            this.setState({trackedS: tags})
+        })
     }
 
+     updateData = (from,to) => {
+            const fromVal = from.split('/').reverse().join("-")
+            const fromDay = fromVal.slice(5,7)
+            const fromYear = fromVal.slice(0,4)
+            const fromMonth = fromVal.slice(8,10)
+            const reverseFrom = `${fromYear}-${fromMonth}-${fromDay}`
+    
+            const toVal = to.split('/').reverse().join("-")
+            const toDay = toVal.slice(5,7)
+            const toYear = toVal.slice(0,4)
+            const toMonth = toVal.slice(8,10)
+            const reverseTo = `${toYear}-${toMonth}-${toDay}`
+
+            const dat1 = [];
+
+            this.state.trackedS.forEach(single => {
+                const collectedTweets = [];
+                const collecredRetweets = []
+                const averageSentimentPolarity = [];
+                const averageSentimentPolarityWithoutRetweets = [];
+                const averageSentimentSubjectivity = [];
+                const countPositive = [];
+                const countNegative = [];
+                const countNeutral = [];
+
+
+                fetch(`${this.props.path}api/getSearchAnalysissAggregatedByDays/collectedTweets/${single.slice(1,single.length)}/${reverseFrom}/${reverseTo}`)
+                .then(resp => resp.json())
+                .then(data => {
+                    collectedTweets.push(data)
+                    return fetch(`${this.props.path}api/getSearchAnalysissAggregatedByDays/collectedRetweets/${single.slice(1,single.length)}/${reverseFrom}/${reverseTo}`)
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    collecredRetweets.push(data)
+                    return fetch(`${this.props.path}api/getSearchAnalysissAggregatedByDays/averageSentimentPolarity/${single.slice(1,single.length)}/${reverseFrom}/${reverseTo}`)
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    averageSentimentPolarity.push(data)
+                    return fetch(`${this.props.path}api/getSearchAnalysissAggregatedByDays/averageSentimentPolarityWithoutRetweets/${single.slice(1,single.length)}/${reverseFrom}/${reverseTo}`)
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    averageSentimentPolarityWithoutRetweets.push(data)
+                    return fetch(`${this.props.path}api/getSearchAnalysissAggregatedByDays/averageSentimentSubjectivity/${single.slice(1,single.length)}/${reverseFrom}/${reverseTo}`)
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    averageSentimentSubjectivity.push(data)
+                    return fetch(`${this.props.path}api/getSearchAnalysissAggregatedByDays/countPositive/${single.slice(1,single.length)}/${reverseFrom}/${reverseTo}`)
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    countPositive.push(data)
+                    return fetch(`${this.props.path}api/getSearchAnalysissAggregatedByDays/countNegative/${single.slice(1,single.length)}/${reverseFrom}/${reverseTo}`)
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    countNegative.push(data)
+                    return fetch(`${this.props.path}api/getSearchAnalysissAggregatedByDays/countNeutral/${single.slice(1,single.length)}/${reverseFrom}/${reverseTo}`)
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    countNeutral.push(data)
+                })
+
+
+                dat1.push({"name": single,
+                           "colTweets": collectedTweets, 
+                           "colRetweets": collecredRetweets,
+                           "averageSentimentPolarity": averageSentimentPolarity,
+                           "averageSentimentPolarityWithoutRetweets": averageSentimentPolarityWithoutRetweets,
+                           "averageSentimentSubjectivity": averageSentimentSubjectivity,
+                           "countPositive": countPositive,
+                           "countNegative": countNegative,
+                           "countNeutral": countNeutral})
+
+            
+            })
+
+            console.log(dat1)
+            this.setState({from: reverseFrom, to: reverseTo, data: dat1})
+    }
+
+    
 
     render(){
+       
         return ( 
             <div className="main__overallPage overall">
                 <h2 className="overall__title">Ogólne wyniki</h2>
@@ -25,7 +122,6 @@ class OverallPage extends Component {
                 <div className="overall_dayPicker">
                     <DaysPicker updateData={this.updateData}/>
                 </div>
-
             </div> 
             );
     }
